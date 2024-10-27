1ccfe4f3597d121d6d5f8ccbbe5584bd0adbab6d
ejtrheoz <comp140609@gmail.com>
2024-10-27 02:30:13 +0100

    model davinci using

--- model/model.py
+++ model/model.py
@@ -1,17 +1,21 @@
+import os
 from dotenv import load_dotenv
 from openai import OpenAI
-
 
 class Client:
     def __init__(self) -> None:
         load_dotenv()
-        self.client = OpenAI()
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     
-    def response(self, text):
-        response = self.client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "user", "content": text}
-        ]
+    def response(self, text, user_data=""):
+
+        full_prompt = f"{user_data}\n{text}" if user_data else text
+
+        response = self.client.completions.create(
+            model="davinci-002",
+            prompt=full_prompt,  
+            max_tokens=100,
+            temperature=0.7
         )
-        return response.choices[0].message.content
+        return response.choices[0].text.strip() 
+
